741e61e20e62303d98668e892cdc4f82272dc6b9
usmon <usmonkarimov0726@gmail.com>
2024-08-09 11:22:56 +0500

    Finished inline-markdown functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,7 +5,17 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_link,
+    text_type_image
 )
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    res = split_nodes_link(nodes)
+    return res
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
@@ -17,7 +27,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         l = node.text.split(delimiter)
         if len(l)%2==0:
             raise Exception('Invalid Markdown')
-        print(l)
+        
         for i in range(len(l)):
             if l[i]=="":
                 continue
@@ -39,3 +49,76 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     links = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)",text)
     return links
+
+
+def split_nodes_link(old_nodes):
+    res=[]
+    pattern = r'\[.*?\]\(.*?\)'
+    pattern_link = r'\[(.*?)\]\((.*?)\)'
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            res.append(node)
+            continue
+        #checking if the text have links
+        links = extract_markdown_links(node.text)
+        if len(links)==0:
+            res.append(node)
+            continue
+        #splitting into parts using pattern
+        parts = re.split(f'({pattern})',node.text)
+        #Itterating through every part
+        for part in parts:
+            if  part =='':
+                continue
+            #Finding mathces for (smt)[smt] stirng
+            match = re.match(pattern_link,part)
+            # 
+            if match:
+                text = match.group(1)
+                url = match.group(2)
+                n = TextNode(text,text_type_link,url)
+                res.append(n)
+            else:
+                n = TextNode(part,text_type_text)
+                res.append(n)
+
+
+    return res
+
+def split_nodes_image(old_nodes):
+    res=[]
+    pattern = r'\[.*?\]\(.*?\)'
+    pattern_link = r'\[(.*?)\]\((.*?)\)'
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            res.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if len(images)==0:
+            res.append(node)
+            continue
+        #splitting into parts using pattern
+        parts = re.split(f'({pattern})',node.text)
+        #Itterating through every part
+        for part in parts:
+            if  part =='!' or part =='':
+                continue
+            #Finding mathces for (smt)[smt] stirng
+            match = re.match(pattern_link,part)
+            # 
+            if match:
+                text = match.group(1)
+                url = match.group(2)
+                n = TextNode(text,text_type_image,url)
+                res.append(n)
+            else:
+                n = TextNode(part[:-1],text_type_text)
+                res.append(n)
+
+
+    return res
+
+
+
+text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+print(text_to_textnodes(text))
